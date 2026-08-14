84f503a1d962a6dab30d87ed4f879553e58b103a
rxw <1120309488@qq.com>
2026-08-14 11:49:35 +0800

    优化界面

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { onBeforeUnmount } from 'vue'
+import { RouterView, useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleRouteChange = (path) => {
+  router.push(path)
+}
+
+window.$wujie?.bus.$on('routeChange', handleRouteChange)
+
+onBeforeUnmount(() => {
+  window.$wujie?.bus.$off('routeChange', handleRouteChange)
+})
 </script>
 
 <template>
-    <RouterView />
+  <RouterView />
 </template>
-
-<style scoped>
-</style>
